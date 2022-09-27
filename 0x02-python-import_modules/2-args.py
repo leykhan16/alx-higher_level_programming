bd6b1d4a9349fd4aff318213cdb7bb98a835a9d5
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-09-28 00:36:37 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    if l == 1:
-        print('0 argument.')
-    if l == 2:
-        print('1 argument:')
-        print('1: {:s}'.format(argv[1]))
-    if l > 2:
-        print('{:d} arguments:'.format(l-1))
-        for i in range(1, l):
-            print('{:d}: {}'.format(i, argv[i]))
+    """Print the number of and list of arguments"""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
